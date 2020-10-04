41c2d55c31c4e90dc53d4e8c799328942e0c3670
Hunnain Atif <td-dev@outlook.com>
2020-10-03 23:29:19 -0400

    parse api for relevant information

--- src/api/index.js
+++ src/api/index.js
@@ -4,8 +4,18 @@ const url = "https://covid19.mathdro.id/api";
 
 export const fetchData = async () => {
   try {
-    const response = await axios.get(url);
-    return response;
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(url);
+
+    const selectedData = {
+      confirmed: confirmed,
+      recovered: recovered,
+      deaths: deaths,
+      lastUpdate: lastUpdate,
+    };
+
+    return selectedData;
   } catch (error) {
     console.log("error gettin response");
   }
