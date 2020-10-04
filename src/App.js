82a59716fb1785e35f7f655a352c4b53cb089165
Hunnain Atif <td-dev@outlook.com>
2020-10-03 23:23:55 -0400

    connecting with api

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import React from "react";
 import Cards from "./components/Cards/Cards";
 import Chart from "./components/Chart/Chart";
 import CountryChooser from "./components/CountryChooser/CountryChooser";
+import { fetchData } from "./api";
 import styles from "./App.module.css";
 
 class App extends React.Component {
+  async componentDidMount() {
+    const data = await fetchData();
+
+    console.log(data);
+  }
+
   render() {
     return (
       <div className={styles.container}>
